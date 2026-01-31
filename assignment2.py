4d52acc8dbd24bef9be16bb903661d44f9c885e7
stotten4 <stotten4@gmu.edu>
2026-01-31 12:31:20 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,42 +1,7 @@
-"""
-Introduction to Programming for Health Applications
-Collaborative Python Assignment
-"""
 
-def main():
-    print("Welcome to the Assignment2.1\n")
-
-    # ==== STUDENT FUNCTION CALLS ====
-    # Add your function call below this line
-    # Example:
-    lastname_firstname()
-    sin_mal()
-
-    # ==== END STUDENT FUNCTION CALLS ====
+def SHA_TOT():
+    print("patient name: Shama Totten")
+    print("patient age: 38years ")
+    print("diagnosis: Hypertension")
 
 
-# ==== STUDENT CODE SECTION ====
-# Each student:
-# 1.To Create ONE function
-# 2. Name it lastname_firstname (Can be your first 3 letters of your last name and first Name Ex: SIN_MAL) 
-# 3. Print a sample data with variables from the given example
-# 4. DO NOT modify other student functions
-
-
-# Example:
-def lastname_firstname():
-    print("Patient Name: John Doe")
-    print("Patient Age: 32 years")
-    print("Diagnosis: Type 2 Diabeties")
-
-
-def sin_mal():
-    print("Patient Name: Mark Edwin")
-    print("Patient Age: 28 years")
-    print("Diagnosis: Type 1 Diabates")
-
-# ==== END STUDENT CODE SECTION ====
-
-
-if __name__ == "__main__":
-    main()
